937a57a65d2f7712ff2a3d3cbb0f6ba86c9975ac
duckfuss <dtfussey@icloud.com>
2025-05-24 11:20:39 +0100

    Implement Graph class with node addition and drawing functionality

--- grapher.py
+++ grapher.py
@@ -1,5 +1,36 @@
-import numpy
+import pygame
+import random
 
 class Graph():
     def __init__(self) -> None:
-        pass
+        # directed graph
+        self.graphDict = {}
+        # pygame boilerplate
+        self.xMax, self.yMax = 1280, 720
+        self.screen = pygame.display.set_mode((self.xMax,self.yMax))
+        self.rects = {}
+
+    def addNode(self, node:str, links:set):
+        self.graphDict[node] = links
+    def draw(self):
+        for node in self.graphDict:
+            xCoord = random.randrange(0,self.xMax)
+            yCoord = random.randrange(0,self.yMax)
+            pygame.draw.circle(self.screen, "red", (xCoord, yCoord), 10)
+
+# test code:
+pygame.init()
+graphy = Graph()
+running = True
+
+graphy.addNode("A", {"B", "C"})
+graphy.addNode("B", {"B", "C"})
+
+graphy.draw()
+pygame.display.flip()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
---
+++ main.py
@@ -0,0 +1,2 @@
+import grapher
+import scraper
