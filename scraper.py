80356091dd88a81a5aaeee2897341269e5cccbed
duckfuss <dtfussey@icloud.com>
2025-05-24 10:51:17 +0100

    Refactor scraper methods for improved clarity and functionality

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 class scraperBot():
@@ -10,28 +10,20 @@ class scraperBot():
         options.add_argument("--height=1440")
         self.browser = webdriver.Firefox(options=options)
 
-    def scrape(self, site, depth=3):
-        self.browser.get(site)
-        for i in range(depth):
-            print("----------depth:", i)
-            link = self.findLink()
-            link.click() # type: ignore
-            time.sleep(1)
-
-    def findLink(self, n=1):
+    def findNode(self, n=1):
         for i in range(1,10):
             path = "//main/div[3]/div[3]/div[1]/p[" + str(i) + "]/a[" +str(n) + "]"
-            try:
-                return self.browser.find_element(By.XPATH, path)
-            except NoSuchElementException:
-                print("it wasn't i =", i)
+            try:    return self.browser.find_element(By.XPATH, path)
+            except NoSuchElementException:  pass
 
+    def scrape(self, startSite, depth=3):
+        self.browser.get(startSite)
+        for i in range(depth):
+            node = self.findNode()
+            link = node.get_attribute("href") # type: ignore
+            print(i, link.replace("https://en.wikipedia.org/wiki/", "")) # type: ignore
+            node.click() # type: ignore
+            time.sleep(0.1)
 
 duck = scraperBot()
 duck.scrape("https://en.wikipedia.org/wiki/Duck",10)
-
-#.mw-content-ltr > p:nth-child(9) > a:nth-child(2)
-#.mw-content-ltr > p:nth-child(5) > a:nth-child(2)
-#/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2]/a[1]
-#/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[3]/a[1]
-#/html/body/div[2]/div/div[3]/main/div[3]/div[3]/div[1]/p[2]/a[1]
