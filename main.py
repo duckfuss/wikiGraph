17653fe455bcd6df2d49d6ffb29db711aa82ed2e
duckfuss <dtfussey@icloud.com>
2025-05-25 14:44:00 +0100

    Refactor main loop and enhance node handling in graph visualization

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ import scraper
 import springSim
 import time
 
-def addNode(node, links):
-    graph.addNode(node, links)
-    sim.introduceNode(node,links)
-
 duck = scraper.Scraper()
 graph = grapher.Graph()
 sim = springSim.Sim()
@@ -20,27 +16,43 @@ startList = [
     "https://en.wikipedia.org/wiki/Rhine_campaign_of_1796",
     "https://en.wikipedia.org/wiki/Ivan_the_Terrible_(polar_bear)",
     "https://en.wikipedia.org/wiki/Taipei",
-    "https://en.wikipedia.org/wiki/Sausage"
+    "https://en.wikipedia.org/wiki/Sausage",
+    "https://en.wikipedia.org/wiki/Special:Random"
 ]
+#for i in range(50):
+#    startList.append("https://en.wikipedia.org/wiki/Special:Random")
 running = True
 start = time.time()
-pageDepth, pagesVisited, SLIndex = 15, 0, 0
+pageDepth, pagesVisited, SLIndex = 20, 0, 0
 node = startList[SLIndex]
+graph.addNode(node, set())
 
-while running:
-    if SLIndex < len(startList):
-        if pagesVisited >= pageDepth:
-            SLIndex += 1
-            if SLIndex < len(startList):
-                node = startList[SLIndex]
-                pagesVisited = 0
-                print("-----------------", len(startList), SLIndex)
-        if SLIndex < len(startList) and (time.time() - start) > 0.1:
-            start = time.time()
-            pagesVisited += 1
-            links = duck.collectLinks(node, breadth=1)
-            addNode(node, links)
-            node = links[0] # type: ignore
-            print(pagesVisited, node)
-    running = sim.updateGraphics()
+
+while SLIndex < len(startList):
+    if pagesVisited >= pageDepth:
+        SLIndex += 1
+        if SLIndex < len(startList):
+            node = startList[SLIndex]
+            graph.addNode(node, set())
+            pagesVisited = 0
+            print("-----------------", len(startList), SLIndex)
+
+    if graph.graphDict[node] == set():  # If the node hasn't been visited
+        links = duck.collectLinks(node, breadth=1)
+        graph.addNode(node, links)  # type: ignore
+        node = links[0]
+        print(pagesVisited, node)
+        pagesVisited += 1
+    else:  # Skip already visited nodes
+        print(f"Skipping already visited page: {node}")
+        pagesVisited += pageDepth  # Ensure program moves on to the next start word
+
 duck.browser.quit()
+
+# Generate graph visualization
+for node, links in graph.graphDict.items():
+    sim.introduceNode(node, links)
+
+# Keep the graphics running in a loop
+while running:
+    running = sim.updateGraphics()
