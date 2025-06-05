672ac0a9e838e7980de2ae06b7fb9f2723ea0195
duckfuss <dtfussey@icloud.com>
2025-06-05 11:06:36 +0100

    Refactor scraper and main logic: update maxSeeds, enhance link collection, and improve orphan handling

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import grapher
 import soupyScraper
 import graphics
 import time
+import asyncio
 
 # change at will:
 #------------------------------------#
-maxSeeds = 1000
+maxSeeds = 10
 pageDepth = 50  # doesn't really matter if noOrphans is set to True
 pageBreadth = 1
 noOrphans = True
@@ -14,63 +15,44 @@ language = "English" # Supported: English, French, Chinese, Japanese, Spanish, L
 #------------------------------------#
 
 # initialise stuff
-duck = soupyScraper.Scraper(language=language)
 graph = grapher.Graph()
+duck = soupyScraper.Scraper(language=language)
 sim = graphics.Sim()
 sim.setGraph(graph)
+duck.setGraph(graph)
 
 # don't change:
 running = True
 start = time.time()
 pagesVisited, SLIndex = 0, 0
 
-
-def exploreLinksAndGraph(startSite, breadth, depth):
-    pagesVisited = 0
-    node = startSite
-    graph.addNode(node, set())
-    print("startSite:", startSite)
-    while pagesVisited < depth:
-        if graph.graphDict[node] == set():  # If node's new
-            print(pagesVisited, node[30:])
-            links = duck.collectLinks(node, breadth=breadth)
-            graph.addNode(node, links) # type: ignore
-             # abort if cannot find a follow-on link:
-            if links:   node = links[0] 
-            else:       return None
-            pagesVisited += 1
-        else:  # Skip already visited nodes
-            print("Skipping already visited page:", node[30:])
-            return pagesVisited
-    return pagesVisited # never used, but could be useful for debugging
-
 # traverse wikipedia
 for seed in range(maxSeeds):
     print("\nTRAVERSING---------------", seed)
-    exploreLinksAndGraph(duck.getRandomPage(), pageBreadth, pageDepth)
+    duck.exploreLinksAndGraph(duck.getRandomPage(), pageBreadth, pageDepth)
 
 if noOrphans:
-    for i in range(500): # functionally while True
+    for i in range(500):  # functionally while True
         print("\n\n\nPROCESSING DEAD END NODES", i)
         orphanNodeList = [node for node, links in graph.graphDict.items() if links == set()]
         deadNodes = 0
         for node in orphanNodeList:
             print("\nDEAD END---------------", node[30:], i)
-            if exploreLinksAndGraph(node, 1, 10) is None:   deadNodes += 1
+            if duck.exploreLinksAndGraph(node, 1, 10) is None:
+                deadNodes += 1
         if len(orphanNodeList) == 0 or len(orphanNodeList) <= deadNodes:
             print("No more orphan nodes to process.", orphanNodeList)
             break
 
-#duck.browser.quit()
 graph.generateParentDict()
 print("DONE - generated", len(graph.graphDict), "nodes")
 
-# Generate graph visualizationx
+# Generate graph visualization
 for node, links in graph.graphDict.items():
     sim.introduceNode(node, links)
 
 # Keep the graphics running in loop
-localSelected = "bob" # ensures update highlight on first loop
+localSelected = "bob"  # ensures update highlight on first loop
 while running:
     running = sim.handleEvents()
     sim.updateGraphics()
--- soupyScraper.py
+++ soupyScraper.py
@@ -1,49 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
-# remember pip3 install lxml
 
 class Scraper():
-    def __init__(self, language = "English") -> None:
+    def __init__(self, language="English") -> None:
         self.language = language
-        self.langDict = {"English":  "https://en.wikipedia.org/wiki/Special:Random",
-                         "French":   "https://fr.wikipedia.org/wiki/Spécial:Page_au_hasard",
-                         "Japanese": "https://ja.wikipedia.org/wiki/特別:おまかせ表示",
-                         "Spanish":  "https://es.wikipedia.org/wiki/Especial:Aleatoria",
-                         "Chinese":  "https://zh.wikipedia.org/wiki/Special:随机页面", # Taiwan style ;)
-                         "Latin":    "https://la.wikipedia.org/wiki/Specialis:Pagina_fortuita",
-                         "Scots":    "https://sco.wikipedia.org/wiki/Special:Random"} # funny sounding
+        self.langDict = {
+            "English":  "https://en.wikipedia.org/wiki/Special:Random",
+            "French":   "https://fr.wikipedia.org/wiki/Spécial:Page_au_hasard",
+            "Japanese": "https://ja.wikipedia.org/wiki/特別:おまかせ表示",
+            "Spanish":  "https://es.wikipedia.org/wiki/Especial:Aleatoria",
+            "Chinese":  "https://zh.wikipedia.org/wiki/Special:随机页面",
+            "Latin":    "https://la.wikipedia.org/wiki/Specialis:Pagina_fortuita",
+            "Scots":    "https://sco.wikipedia.org/wiki/Special:Random"
+        }
+        self.ignoreList = [
+            "Old French", "Latin", "Greek", "Ancient Greek", "Grec ancien",
+            "Simplified Chinese characters", "Chinese characters", "Traditional Chinese characters",
+            "Pinyin", "Help:Pronunciation respelling key", "American English", "Espagnol",
+            "Grec (langue)", "language", "langue", "lengua", "言語", "語言", "wikt", "Help:", "Play audio"
+        ]
 
-        #ignore list (manual and mainly english)
-        self.ignoreList = ["Old French", "Latin", "Greek", "Ancient Greek", "Grec ancien", "Simplified Chinese characters", "Chinese characters", "Traditional Chinese characters", "Pinyin", "Help:Pronunciation respelling key", "American English", "Espagnol", "Grec (langue)", "language", "langue", "lengua", "言語", "語言"]
+    def setGraph(self, graph):
+        self.graph = graph
 
     def getRandomPage(self):
         r = requests.get(self.langDict[self.language])
         return r.url
-
-    def getAllLinks(self, site):
-        linkStarter = self.langDict[self.language].split("/wiki")[0]
-        linkList = []
-        r = requests.get(site)
-        soup = BeautifulSoup(r.text, "lxml")
-        for paragraph in soup.find_all("p"):
-            for link in paragraph.find_all("a"): # type: ignore
-                if link.get("href") is None or link.find_parent("sup"): # type: ignore
-                    continue
-                linkList.append(linkStarter + link.get("href")) # type: ignore
-        return linkList
 
     def collectLinks(self, site, breadth=1):
         linkStarter = self.langDict[self.language].split("/wiki")[0]
         linkList = []
         r = requests.get(site)
         soup = BeautifulSoup(r.text, "lxml")
-        
-        # Iterate through paragraphs and find links
         for paragraph in soup.find_all("p"):
             for link in paragraph.find_all("a"): # type: ignore
-                if link.get("href") is None or link.find_parent("sup"): # type: ignore
+                if link.get("href") is None or link.find_parent("sup") or link.find_parent("tr") or link.find_parent("span"): # type: ignore
+                    # ignore None and superscript (citations)
                     continue
-                linkList.append(linkStarter + link.get("href"))  # type: ignore
-                if len(linkList) >= breadth:  # Stop once we have enough links
+                if link.get("class") and "external" in link.get("class"): # type: ignore
+                    # ignore external links
+                    continue
+                if link.get("title") and any(ig in link.get("title") for ig in self.ignoreList): # type: ignore
+                    continue
+                linkList.append(linkStarter + link.get("href")) # type: ignore
+                if len(linkList) >= breadth:
                     return linkList
-        return linkList  # Return collected links (may be fewer than breadth if not enough links found)
+        return linkList
+
+    def exploreLinksAndGraph(self, startSite, breadth, depth):
+        pagesVisited = 0
+        node = startSite
+        self.graph.addNode(node, set())
+        print("startSite:", startSite)
+        while pagesVisited < depth:
+            if self.graph.graphDict[node] == set():  # If node's new
+                print(pagesVisited, node[30:])
+                links = self.collectLinks(node, breadth=breadth)
+                self.graph.addNode(node, links)
+                # Abort if cannot find a follow-on link:
+                if links:   node = links[0]
+                else:       return None
+                pagesVisited += 1
+            else:  # Skip already visited nodes
+                print("Skipping already visited page:", node[30:])
+                return pagesVisited
+        return pagesVisited
